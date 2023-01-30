2738039d38c1b3f5a483b6b4658756ce4cb6e20c
Erik Gebhard <89320016+gebhaard@users.noreply.github.com>
2023-01-30 16:42:06 +0100

    moved from bin to root

--- BayerPropertyCrawler.py
+++ BayerPropertyCrawler.py
@@ -5,13 +5,12 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 from datetime import datetime
 
-# input to choose project you want to scrape
 
 with open('projects.json', 'r', encoding='utf-8') as f:
     project_dict = json.load(f)
     f.close()
 
-project_list = {key:val['short'] for key, val in project_dict.items()}
+project_list = {key: val['short'] for key, val in project_dict.items()}
 
 print('You can choose from the following projects:')
 for key, val in project_list.items():
@@ -28,13 +27,14 @@ for key, value in project_dict.items():
         break
 
 if project != value['short']:
-    print('Choose a valid project! Choose from EP, BG or ZL.')
+    print('Choose a valid project! Choose from the list above.')
 
 else:
     pass
 
 r = requests.get(url)
-r = r.text.split('<section class="section section-project-details section-project-details-homes" id="section-homes">')[1]
+r = r.text.split(
+    '<section class="section section-project-details section-project-details-homes" id="section-homes">')[1]
 soup = BeautifulSoup(r, 'html.parser')
 
 ran = range(len(soup.find_all('meta', attrs={'itemprop': 'position'})))
@@ -77,4 +77,5 @@ df_final = pd.merge(df, df2, left_on='Lakás', right_on='name', how='left')
 df_final.drop('name', axis=1, inplace=True)
 
 datetime_now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-df_final.to_excel(f'BayerProperty_{short}_Export_{datetime_now}.xlsx', index=False)
+df_final.to_excel(
+    f'BayerProperty_{short}_Export_{datetime_now}.xlsx', index=False)
